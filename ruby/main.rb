e5522103309b8f41df556f1355581e12293f2e3d
Justin Rhoades <rhoades.justin@gmail.com>
2020-04-27 15:24:24 -0400

    add oj for json and ox for xml, use nokogiri dom parsing

--- ruby/main.rb
+++ ruby/main.rb
@@ -3,6 +3,8 @@ require 'ruby-prof'
 require 'nokogiri'
 require 'memory_profiler'
 require 'json'
+require 'oj'
+require 'ox'
 
 json = File.read('fixtures/offers.json')
 xml = File.read('fixtures/offers.xml')
@@ -21,12 +23,26 @@ end
 def transform(offers)
   offers.map do |o|
     {
-      id: o["id"],
-      name: o["name"],
-      price: o.dig("shortTermPrice", "amount") || o.dig("longTermPrice", "amount"),
-      promotion: o["promotions"],
-      highlight: o["highlights"],
-      provider: o["provider"]["name"]
+      id: o['id'],
+      name: o['name'],
+      price:
+        o.dig('shortTermPrice', 'amount') || o.dig('longTermPrice', 'amount'),
+      promotion: o['promotions'],
+      highlight: o['highlights'],
+      provider: o['provider']['name']
+    }
+  end
+end
+
+def transform_symbol(offers)
+  offers.map do |o|
+    {
+      id: o[:id],
+      name: o[:name],
+      price: o.dig(:shortTermPrice, :amount) || o.dig(:longTermPrice, :amount),
+      promotion: o[:promotions],
+      highlight: o[:highlights],
+      provider: o[:provider][:name]
     }
   end
 end
@@ -34,29 +50,79 @@ end
 puts '------------------------------------------------------------'
 puts '[JSON]'
 profile do
-  offers = JSON.parse(json)["payload"]["offers"]
+  offers = JSON.parse(json)['payload']['offers']
   transform(offers)
 end
 
 puts '------------------------------------------------------------'
 puts '[JSON]'
 MemoryProfiler.start
-offers = JSON.parse(json)["payload"]["offers"]
+offers = JSON.parse(json)['payload']['offers']
 transform(offers)
 report = MemoryProfiler.stop
 report.pretty_print
 
 puts '------------------------------------------------------------'
-puts '[XML]'
+puts '[Oj]'
 profile do
-  offers = Hash.from_xml(xml)["root"]["payload"]["offers"]
-  transform(offers)
+  offers = Oj.load(json)
+  transform(offers['payload']['offers'])
 end
 
 puts '------------------------------------------------------------'
-puts '[XML]'
+puts '[Oj]'
 MemoryProfiler.start
-offers = Hash.from_xml(xml)["root"]["payload"]["offers"]
-transform(offers)
+offers = Oj.load(json)
+transform(offers['payload']['offers'])
+report = MemoryProfiler.stop
+report.pretty_print
+
+puts '------------------------------------------------------------'
+puts '[Nokogiri]'
+profile do
+  offers = Nokogiri::XML.parse(File.open('fixtures/offers.xml'))
+  ok =
+    offers.xpath('//offers').map do |offer|
+      {
+        id: offer.at_xpath('id').text,
+        name: offer.at_xpath('name').text,
+        price:
+          offer.at_css('shortTermPrice amount')&.text ||
+            offer.at_css('longTermPrice amount')&.text
+      }
+    end
+  ok
+end
+
+puts '------------------------------------------------------------'
+puts '[Nokogiri]'
+MemoryProfiler.start
+offers = Nokogiri::XML.parse(File.open('fixtures/offers.xml'))
+ok =
+  offers.xpath('//offers').map do |offer|
+    {
+      id: offer.at_xpath('id').text,
+      name: offer.at_xpath('name').text,
+      price:
+        offer.at_css('shortTermPrice amount')&.text ||
+          offer.at_css('longTermPrice amount')&.text
+    }
+  end
+ok
+report = MemoryProfiler.stop
+report.pretty_print
+
+puts '------------------------------------------------------------'
+puts '[ox]'
+profile do
+  doc = Ox.load(xml, mode: :hash)
+  transform_symbol(doc[:root][:payload][:offers])
+end
+
+puts '------------------------------------------------------------'
+puts '[ox]'
+MemoryProfiler.start
+doc = Ox.load(xml, mode: :hash)
+transform_symbol(doc[:root][:payload][:offers])
 report = MemoryProfiler.stop
 report.pretty_print
